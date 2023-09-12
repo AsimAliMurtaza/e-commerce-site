a7ac46159e582b7a3e14787718cbec77bbea4be3
Asim Ali <mo.asim.murtaza@gmail.com>
2023-09-12 21:34:36 +0500

    added scss to  add to cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,20 +1,22 @@
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
-import './cart-dropdown.styles.scss';
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import "./cart-dropdown.styles.scss";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = ()=>{
-    const {cartItems} = useContext(CartContext);
-    return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-            {cartItems.map(item=> <CartItem cartItem={item}/>)}
-            </div>
-            <Button>CHECKOUT</Button>
-        </div>
-    );
-}
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem cartItem={item} />
+        ))}
+      </div>
+      <Button>CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,22 +1,23 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
-
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
 
-const ProductCard = ({product}) => {
-  const {addItemToCart} = useContext(CartContext);
-  const {price, name, imageUrl} = product;
-  const addProductToCart = ()=> addItemToCart(product);
-  return(
+const ProductCard = ({ product }) => {
+  const { addItemToCart } = useContext(CartContext);
+  const { price, name, imageUrl } = product;
+  const addProductToCart = () => addItemToCart(product);
+  return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button onClick={addProductToCart} buttonType="inverted">Add to cart</Button>
+      <Button onClick={addProductToCart} buttonType="inverted">
+        Add to cart
+      </Button>
     </div>
   );
 };
